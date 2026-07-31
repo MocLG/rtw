6242312b042dc4b0a1f209e5741cab0660fdfd26
Luka Gejak <luka.gejak@linux.dev>
2026-07-31 07:58:24 +0000

    EXPERIMENT: also drop the RTL8723BS coex work and GNT_BT
    On top of the no-join-sequence experiment, reverts the coex scan
    antenna workaround, the associate-time antenna reassertion and the
    GNT_BT firmware command (whose only caller was the coex code).
    Register defines used by the chip driver are kept.
    Not for submission.
    
    Signed-off-by: Luka Gejak <luka.gejak@linux.dev>

--- drivers/net/wireless/realtek/rtw88/coex.h
+++ drivers/net/wireless/realtek/rtw88/coex.h
@@ -430,6 +430,4 @@ static inline void rtw_coex_active_query_bt_info(struct rtw_dev *rtwdev)
 		rtw_coex_query_bt_info(rtwdev);
 }
 
-void rtw_coex_8723bs_pre_auth_h2c(struct rtw_dev *rtwdev);
-
 #endif
